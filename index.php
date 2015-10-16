fc2dcc99e14e15cb4a90947e2e6eaac1a0d18506
Bryson Breivik <brysonbreivik@gmail.com>
2015-10-16 16:16:13 -0600

    Adding what I need
    Yay!

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Home";
+$content = '<img src="images/banner.jpg" class="imgLeft"/>
+<h3>Glamour Essence Spa!</h3>
+<p>
+    This is where you should add some content... I wish I could just come up with what to say, but I don\'t wanna write something you don\'t want in here.
+    So give some thought to what you want your home page to say, and we can add photos, and style it however you\'d like :)
+</p>
+<img src="images/banner.jpg" class="imgRight"/>
+
+<p>
+    This is where you should add some content... I wish I could just come up with what to say, but I don\'t wanna write something you don\'t want in here.
+    So give some thought to what you want your home page to say, and we can add photos, and style it however you\'d like :)
+</p>';
+
+include 'Template.php';
+
+?>
